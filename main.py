ee0b6833ff07caa149e4b5a1035e51b091ec3ba0
dddd-onigiri <dddd.sailing470@gmail.com>
2022-12-12 00:49:46 +0900

    release

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import openai
 import streamlit as st
-st.set_page_config(page_title="AI対話システム", layout="wide")
 
-openai.api_key = "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"
+st.set_page_config(page_title="AI対話システム", layout="wide")
 
 st.title("ChatGPT（AI対話システム）")
 st.caption("Created by Daiki Ito")
 st.write("こんにちは！何でも聞いてください（あくまで参考にね）")
 
-#質問フォーム
+# 質問フォーム
 with st.form(key='input_form'):
+    input_apikey = st.text_input("APIキーを取得してください")
     input_text = st.text_input("質問を入力してください")
     submitted = st.form_submit_button('質問する')
 
 if submitted:
     with st.spinner("考え中…"):
+        openai.api_key = input_apikey
         response = openai.Completion.create(
             engine="text-davinci-003",
             prompt=input_text,
