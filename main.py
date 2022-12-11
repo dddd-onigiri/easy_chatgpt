507c8c7fbf2e86f10eee2964bf1161092ad2ecc7
dddd-onigiri <dddd.sailing470@gmail.com>
2022-12-12 00:30:09 +0900

    release

---
+++ main.py
@@ -0,0 +1,29 @@
+import openai
+import streamlit as st
+
+openai.api_key = "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"
+
+st.title("ChatGPT（AI対話システム）")
+st.caption("Created by Daiki Ito")
+st.write("こんにちは！何でも聞いてください（あくまで参考にね）")
+
+with st.form(key='input_form'):
+    input_text = st.text_input("質問を入力してください")
+    submitted = st.form_submit_button('質問する')
+
+if submitted:
+    with st.spinner("考え中…"):
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=input_text,
+            temperature=0.5,
+            max_tokens=1024,
+            top_p=1.0,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+
+    st.write("返答:", response['choices'][0]['text'])
+
+st.write('ご意見・ご要望は→', 'https://forms.gle/ADcoMh1zQUnoZVub7', 'まで')
+st.write('© 2022 Daiki Ito. All Rights Reserved.')
